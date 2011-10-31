f9a943e1d4e067a6305809ce8fda6509b59a37dd
Karl Seguin <karlseguin@gmail.com>
2011-10-31 21:55:38 +0800

    common file uses global settings

--- tools/scraper/common.rb
+++ tools/scraper/common.rb
@@ -1,11 +1,7 @@
-require 'redis'
 require 'json/ext'
+require File.dirname(__FILE__) + '/../../lib/store'
 #require 'rubberband'
 #require 'sanitize'
-
-
-$redis = Redis.new
-$redis.select(5)
 
 #$es = ElasticSearch.new('127.0.0.1:9200', :index => "jobs", :type => "job")
 
@@ -14,14 +10,17 @@ module Store
     key = "#{job[:source]}:#{job[:source_id]}"
     full_key = "job:#{key}"
     serialized_job = job.to_json
-    if $redis.exists(full_key)
-      $redis.set(full_key, serialized_job)
+    if @@redis.exists(full_key)
+      @@redis.set(full_key, serialized_job)
     else
-      $redis.multi do
-        $redis.set(full_key, serialized_job)
-        $redis.zadd('jobs', job[:created_at].to_i, full_key)
+      @@redis.multi do
+        @@redis.set(full_key, serialized_job)
+        @@redis.zadd('jobs', job[:created_at].to_i, full_key)
       end
     end
     #$es.index({:description => Sanitize.clean(job[:description]), :location => job[:location]}, :id => key)
   end
 end
+
+
+Store.setup
