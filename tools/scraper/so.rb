0406a8eee51706463c9f36043fbee556e45b748d
Karl Seguin <karlseguin@gmail.com>
2012-05-12 10:35:42 +0800

    log so rss to debug broken links

--- tools/scraper/so.rb
+++ tools/scraper/so.rb
@@ -2,6 +2,7 @@ require_relative('common')
 require 'rss/2.0'
 require 'time'
 require 'net/http'
+require 'securerandom'
 
 def extract_ids(guid)
   return nil if guid =~ /careers.stackoverflow.com/
@@ -22,16 +23,18 @@ end
 
 
 response = Net::HTTP.get_response(URI.parse('http://careers.stackoverflow.com/jobs/feed'))
-rss = RSS::Parser.parse(response.body, false)
+body = response.body
+File.write(Time.now.utc.strftime('%Y%m%d_%H%M%S_' + SecureRandom.uuid + '.so'), body)
+rss = RSS::Parser.parse(body, false)
 rss.items.each do |job|
   id_parts = extract_ids(job.guid.to_s)
   next unless id_parts
-  
+
   data = {:created_at => job.date.utc.to_i, :description => job.description, :url => job.link, }
-  
+
   data[:source] = id_parts[0]
   data[:source_id] = id_parts[1]
-  
+
   title_parts = extract_title_parts(job.title)
   data[:title] = title_parts[0]
   data[:company] = title_parts[1]
